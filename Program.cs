2694c14118180312506bf739c0424a403ed199de
Jonathon Ireland <59446203+jonathonireland@users.noreply.github.com>
2024-05-20 18:23:33 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("*************** - Lists - *****************");
+// Declare a List 
+
+// Add values to list
+
+// Print values in list - for
+
+// Print values list - foreach
+
+
